62a99f985775b48943eb93e807266adb423b94cc
robbsnor <robbinschmidt@gmail.com>
2024-03-09 13:04:15 +0100

    -

--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -19,7 +19,6 @@ const cards = ref<CardVideoType[]>()
 
 onMounted(async () => {
     const userLogin = route.params.userLogin as string;
-    console.log(userLogin)
     user.value = (await twitchService.getUsers({ logins: [userLogin] }))[0];
     videos.value = await twitchService.getVideos(Number(user.value.id));
     cards.value = CardVideoFactory.mapToCardVideo(videos.value);
