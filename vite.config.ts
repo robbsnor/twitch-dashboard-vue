776476f2af21c345dea49b1ebf6345677ecee489
robbsnor <robbinschmidt@gmail.com>
2024-03-10 22:37:06 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,22 @@
 import { fileURLToPath, URL } from 'node:url';
-
+import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                theme_color: '#000000',
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     }
-  }
 });
