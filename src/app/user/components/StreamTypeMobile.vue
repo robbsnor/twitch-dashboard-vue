871afb79b7df3fa2a8122f260f439f574d832a3a
robbsnor <robbinschmidt@gmail.com>
2024-03-12 23:39:06 +0100

    -

--- src/app/user/components/StreamTypeMobile.vue
+++ src/app/user/components/StreamTypeMobile.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="stream_types_placing">
         <div class="stream_types">
-            <div class="stream_type">
+            <h3 class="stream_type">
                 Highlights
                 <vue-feather type="film" class="stream_type_icons stream_type_icons_right_aligned"></vue-feather>
-            </div>
+            </h3>
 
-            <div class="stream_type">
+            <h3 class="stream_type">
                 Streams
                 <vue-feather type="play" class="stream_type_icons stream_type_icons_left_aligned "></vue-feather>
-            </div>
+            </h3>
 
 
-            <div class="stream_type">
+            <h3 class="stream_type">
                 Clips
                 <vue-feather type="paperclip" class="stream_type_icons stream_type_icons_right_aligned"></vue-feather>
-            </div>
+            </h3>
 
         </div>
     </div>
@@ -38,11 +38,12 @@
     flex-direction: column;
     align-items: end;
     gap: 20px;
-    font-size: 48px;
     color: $c-white;
 }
 
 .stream_type {
+    font-size: 48px;
+    padding: 0;
     position: relative;
 }
 
