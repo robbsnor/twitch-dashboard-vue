e69ac1a50736866737b3414f38566b5f0fc6582a
robbsnor <robbinschmidt@gmail.com>
2024-03-15 12:43:41 +0100

    -

--- src/app/shared/services/title.service.ts
+++ src/app/shared/services/title.service.ts
@@ -3,6 +3,6 @@ import { useTitle } from "@vueuse/core";
 export class TitleService {
     public static setTitle(newTitle: string) {
         const title = useTitle();
-        title.value = `Twitch Dashboard | ${newTitle}`; // change current title
+        title.value = `Twitch Dashboard | ${newTitle}`;
     }
 }
