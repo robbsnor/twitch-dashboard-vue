d63396145461f90e6aacbc4b4e7e9045feb7bc6d
robbsnor <robbinschmidt@gmail.com>
2024-03-23 02:00:52 +0100

    -

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,12 @@
 import { autoAnimatePlugin } from '@formkit/auto-animate/vue';
 import { createPinia } from 'pinia';
-import PrimeVue from 'primevue/config';
 import { createApp } from 'vue';
 import VueFeather from 'vue-feather';
 import App from './App.vue';
 import router from './router/router';
 
-// prime
-import 'primevue/resources/themes/aura-dark-lime/theme.css';
-import 'primeicons/primeicons.css';
-
-import Button from 'primevue/button';
-import Card from 'primevue/card';
-import Checkbox from 'primevue/checkbox';
-import InputText from 'primevue/inputtext';
-import MultiSelect from 'primevue/multiselect';
-import Slider from 'primevue/slider';
-import Sidebar from 'primevue/sidebar';
+import ElementPlus from 'element-plus';
+import 'element-plus/theme-chalk/dark/css-vars.css';
 
 import './assets/styles/main.scss';
 
@@ -26,16 +16,7 @@ const pinia = createPinia();
 app.use(pinia);
 app.use(router);
 app.use(autoAnimatePlugin);
-
-// prime
-app.use(PrimeVue);
-app.component('Button', Button);
-app.component('Card', Card);
-app.component('Checkbox', Checkbox);
-app.component('InputText', InputText);
-app.component('MultiSelect', MultiSelect);
-app.component('Slider', Slider);
-app.component('Sidebar', Sidebar);
+app.use(ElementPlus);
 
 app.component(VueFeather.name, VueFeather);
 
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,33 +1,102 @@
 <script setup lang="ts">
 import UserHeader from '../app/user/components/UserHeader.vue';
 import TempTabs from '../app/user/components/TempTabs.vue';
-import DrawerFilter from '../app/user/components/DrawerFilter.vue';
-import VueFeather from 'vue-feather';
-import { ref } from 'vue';
+import ButtonGroup from '../app/shared/components/ButtonGroup.vue';
+import Button from '../app/shared/components/Button.vue';
+import { LEKKER_SPELEN_VIDEOS } from '../app/user/data/lekkerspelen-videos.data';
+import { computed, onMounted, ref } from 'vue';
 
-const _showDrawer = ref(false);
+const _drawer = ref(false)
+const _category = ref();
+const _search = ref();
+const _videoTypes = ref();
+const _spoilers = ref();
+const _additionalVideoInfo = ref(LEKKER_SPELEN_VIDEOS);
 
-const toggleDrawer = () => _showDrawer.value = !_showDrawer.value;
+const _categories = computed(() => {
+    const duplicateCategories = _additionalVideoInfo.value.map((video) => video.chapters.map(chapter => chapter.title))
+    const orderedCategories = [...new Set(duplicateCategories.flat())].filter(category => category !== "").sort();
+    return orderedCategories;
+})
+
+const _categoryOptions = computed(() => {
+    return _categories.value.map((category) => {
+        return {
+            value: category,
+            label: category
+        }
+    })
+})
+
+onMounted(() => {
+})
 </script>
 
 <template>
-    <div class="user">
-        <UserHeader class="user__header" />
+    <UserHeader />
+    <TempTabs />
 
-        <div class="user__container">
-            <TempTabs />
-            <VueFeather @click="toggleDrawer" type="arrow-down-right" />
-        </div>
+    <el-input
+        v-model="_search"
+        placeholder="Search videos..."
+    >
+        <template #suffix>
+            <vue-feather @click="_drawer = true" type="heart"></vue-feather>
+        </template>
+    </el-input>
 
-        <Sidebar
-            v-model:visible="_showDrawer"
-            header="Search"
-            position="bottom"
-            class="drawer-bottom-small"
-        >
-            <DrawerFilter @toggle-drawer="toggleDrawer" />
-        </Sidebar>
-    </div>
+    <el-drawer
+        v-model="_drawer"
+        size="auto"
+        direction="btt"
+    >
+        <template #header>
+            <h4>Filter videos</h4>
+        </template>
+
+        <template #default>
+            <div class="filter-drawer">
+                <div class="filter-drawer__section section">
+                    <h3 class="section__title">Category</h3>
+                    <div class="section__body">
+                        <el-select-v2
+                            v-model="_category"
+                            filterable
+                            :options="_categoryOptions"
+                            placeholder="Please select"
+                        />
+                    </div>
+                </div>
+
+                <div class="filter-drawer__section section">
+                    <h3 class="section__title">Video types</h3>
+                    <div class="section__body">
+                        <el-checkbox v-model="_videoTypes" label="Streams" size="large" />
+                        <el-checkbox v-model="_videoTypes" label="Highlights" size="large" />
+                        <el-checkbox v-model="_videoTypes" label="Clips" size="large" />
+                    </div>
+                </div>
+
+                <div class="filter-drawer__section section">
+                    <h3 class="section__title">Spoilers</h3>
+                    <div class="section__body">
+                        <el-checkbox v-model="_spoilers" label="Show time" size="large" />
+                        <el-checkbox v-model="_spoilers" label="Show thumbnail" size="large" />
+                    </div>
+                </div>
+
+                <div class="filter-drawer__footer">
+                </div>
+            </div>
+        </template>
+
+        <template #footer>
+            <ButtonGroup>
+                <Button color="secondary">Cancel</Button>
+                <Button>Apply</Button>
+            </ButtonGroup>
+        </template>
+    </el-drawer>
 </template>
 
 <style scoped lang="scss">
@@ -45,6 +114,32 @@ const toggleDrawer = () => _showDrawer.value = !_showDrawer.value;
         @include container
     }
 }
+
+
+.filter-drawer {
+    &__section {
+        padding-bottom: rem(30px);
+    }
+
+    &__footer {
+        display: flex;
+        justify-content: flex-end;
+    }
+}
+
+.section {
+    &__title {
+        font-size: rem(18px);
+        padding-bottom: rem(10px);
+    }
+
+    &__body {
+        .el-checkbox {
+            display: flex;
+        }
+    }
+}
+
 
 // @include screen(1400px) {
 //     &__body {
