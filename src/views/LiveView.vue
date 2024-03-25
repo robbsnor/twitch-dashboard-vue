76a131dd6ac8146447fbe08adc71929da785f16e
robbsnor <robbinschmidt@gmail.com>
2024-03-25 21:06:48 +0100

    -

--- src/views/LiveView.vue
+++ src/views/LiveView.vue
@@ -1,44 +1,39 @@
 <script setup lang="ts">
-import { useAuthStore } from '@/app/auth/stores/auth.store';
 import FavouriteStreams from '@/app/live/components/FavouriteStreams.vue';
 import NonFavouriteStreams from '@/app/live/components/NonFavouriteStreams.vue';
 import { LiveService } from '@/app/live/services/live.service';
 import Spinner from '@/app/shared/components/Spinner.vue';
 import ZigZag from '@/app/shared/components/ZigZag.vue';
-import { MOCK_FAVOURITES_FLUUMP, MOCK_FAVOURITES_HOPP } from '@/app/shared/mock-data/favourites.mock';
 import { onMounted, ref } from 'vue';
 import type { TwitchFollowedStreamWithUser } from '../app/shared/models/twitch/followed-streams.model';
-import { TwitchService } from '../app/shared/services/twitch.service';
 import { TitleService } from '../app/shared/services/title.service';
+import { TwitchService } from '../app/shared/services/twitch.service';
 import { useFavouriteStore } from '../app/shared/stores/favourites.store';
 
 TitleService.setTitle('Live');
 const twitchService = new TwitchService();
 const favourtieStore = useFavouriteStore();
 
-const authStore = useAuthStore();
 
-const favouritesList = ref<number[]>();
-
-const allStreams = ref<TwitchFollowedStreamWithUser[]>();
-const favouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
-const nonFavouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
+const _allStreams = ref<TwitchFollowedStreamWithUser[]>();
+const _favouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
+const _nonFavouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
 
 onMounted(async () => {
-    favouritesList.value = favourtieStore.getFavourites();
+    const favouriteIds = favourtieStore.getFavourites();
 
-    allStreams.value = await twitchService.getFollowedStreamsWithUsers();
-    favouriteStreams.value = LiveService.orderFavorites(favouritesList.value, allStreams.value);
-    nonFavouriteStreams.value = allStreams.value.filter(stream => !favouritesList.value?.includes(Number(stream.id)));
+    _allStreams.value = await twitchService.getFollowedStreamsWithUsers();
+    _favouriteStreams.value = LiveService.orderFavorites(favouriteIds, _allStreams.value);
+    _nonFavouriteStreams.value = _allStreams.value.filter(stream => !favouriteIds?.includes(Number(stream.id)));
 })
 </script>
 
 <template>
     <div class="live">
-        <template v-if="allStreams">
-            <FavouriteStreams :streams="favouriteStreams" />
+        <template v-if="_allStreams">
+            <FavouriteStreams :streams="_favouriteStreams" />
             <ZigZag />
-            <NonFavouriteStreams :streams="nonFavouriteStreams" />
+            <NonFavouriteStreams :streams="_nonFavouriteStreams" />
         </template>
 
         <Spinner v-else padding />
