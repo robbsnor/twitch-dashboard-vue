46c5cdedeb734cf3977234dd8637b9cc35d3b959
robbsnor <robbinschmidt@gmail.com>
2024-03-29 13:53:22 +0100

    -

--- src/app/shared/components/Swirl.vue
+++ src/app/shared/components/Swirl.vue
@@ -1,6 +1,6 @@
 <template>
     <svg class="swirl" viewBox="0 0 1920 477" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M-219 421.776C906 -815.724 1444 1168.28 2236.5 198.775" stroke="#8EFD02" stroke-width="2"/>
+    <path d="M-219 421.776C906 -815.724 1444 1168.28 2236.5 198.775" stroke="#cdf620" stroke-width="2"/>
     </svg>
 </template>
 
