0b86221a29837bbd4e6e6fa4aeb3d64cee496a51
robbsnor <robbinschmidt@gmail.com>
2024-04-02 18:56:43 +0200

    -

--- src/app/following/stores/live.store.ts
+++ src/app/following/stores/live.store.ts
@@ -21,8 +21,8 @@ export const useLiveStore = defineStore('live', () => {
 
     const getAllStreams = async () => {
         return new TwitchService().getFollowedStreamsWithUsers();
-        if (!_allStreams.value) _allStreams.value = await new TwitchService().getFollowedStreamsWithUsers();
-        return _allStreams.value;
+        // if (!_allStreams.value) _allStreams.value = await new TwitchService().getFollowedStreamsWithUsers();
+        // return _allStreams.value;
     };
 
     return {
