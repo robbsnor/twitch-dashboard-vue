953e517d97fb0521ecadd6f70855f535156e17bb
robbsnor <robbinschmidt@gmail.com>
2024-04-06 14:36:24 +0200

    -

--- src/app/user/factories/user.factory.ts
+++ src/app/user/factories/user.factory.ts
@@ -25,9 +25,14 @@ export class UserFactory {
     }
 
     public static mapToUserHeader(user: TwitchUser, followers: number): UserHeaderProps {
+        const banner = user.offline_image_url
+            ? user.offline_image_url
+            : 'https://static-cdn.jtvnw.net/jtv_user_pictures/340bf118-4558-4d0a-8285-166043494a37-profile_banner-480.jpeg';
+        // : 'https://images.unsplash.com/photo-1584204559709-ca7d413229eb?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
         return {
             username: user.display_name,
-            banner: user.offline_image_url ?? '',
+            banner: banner,
             avatar: user.profile_image_url,
             followers: followers,
             isFavourite: false,
