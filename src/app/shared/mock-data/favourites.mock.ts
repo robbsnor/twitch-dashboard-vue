063b9ad5cbeec30f7128731cb83b2fe0182f76a0
robbsnor <robbinschmidt@gmail.com>
2024-05-23 18:30:44 +0200

    -

--- src/app/shared/mock-data/favourites.mock.ts
+++ src/app/shared/mock-data/favourites.mock.ts
@@ -2,9 +2,9 @@
 export const MOCK_FAVOURITES_HOPP = [
     52385053, // lekkerspelen
     128856353, // mande
-    26991127, // hiko
     101936909, // stormen
     51929371, // ml7support
+    26991127, // hiko
     27765748, // bumpaah
     23220337, // emongg
     75738685, // insym
