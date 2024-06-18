f83a522bf05a3357dcb0248bf60583c274c22458
robbsnor <robbinschmidt@gmail.com>
2024-06-18 23:49:05 +0200

    -

--- src/app/following/components/CardLive.vue
+++ src/app/following/components/CardLive.vue
@@ -38,7 +38,7 @@ const viewers = computed(() => {
         </div>
         <div class="card-small__info">
             <div class="card-small__title">{{ card.title }}</div>
-            <button class="card-small__game" @click="emits('click:game', card.game)">{{ card.game }}</button>
+            <a class="card-small__game" @click.prevent="emits('click:game', card.game)">{{ card.game }}</a>
             <RouterLink :to="`/user/${card.name}`" class="card-small__user">
                 <img v-if="card.avatar" :src="card.avatar" class="card-small__avatar" alt="avatar">
                 <div class="card-small__username">{{ card.name }}</div>
@@ -57,7 +57,7 @@ const viewers = computed(() => {
             <div class="card-normal__viewers">{{ viewers }}</div>
         </a>
         <div class="card-normal__title">{{ card.title }}</div>
-        <button class="card-normal__game" @click="emits('click:game', card.game)">{{ card.game }}</button>
+        <a class="card-normal__game" @click.prevent="emits('click:game', card.game)">{{ card.game }}</a>
         <div class="card-normal__footer">
             <RouterLink :to="`/user/${card.name}`" class="card-normal__user">
                 <img v-if="card.avatar" :src="card.avatar" class="card-normal__avatar" alt="avatar">
