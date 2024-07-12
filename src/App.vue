fc0721ade34f8b496bf0f112ebdc101f560d7244
robbsnor <robbinschmidt@gmail.com>
2024-07-12 22:37:34 +0200

    -

--- src/App.vue
+++ src/App.vue
@@ -79,7 +79,7 @@ const getMainMinHeight = () => {
         // background-image: url('https://plus.unsplash.com/premium_photo-1676272747130-348694463771?q=80&w=3538&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D');
         background-position: center;
         filter: blur(100px);
-        opacity: .35;
+        opacity: .3;
     }
 
     &__header {
