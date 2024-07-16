0c4fe57b7450626a2c4957215f573086aba4d597
robbsnor <robbinschmidt@gmail.com>
2024-07-16 18:01:13 +0200

    -

--- src/app/following/containers/LivePage.vue
+++ src/app/following/containers/LivePage.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, onMounted, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { TitleService } from '../../shared/services/title.service';
 import { useFavouriteStore } from '../../shared/stores/favourites.store';
 import type { TwitchFollowedStreamWithUser } from '../../shared/models/twitch/followed-streams.model';
@@ -9,11 +9,12 @@ import NonFavouriteStreams from '../components/NonFavouriteStreams.vue';
 import { TwitchApiService } from '@/app/shared/services/twitch-api.service';
 import { useFollowingStore } from '../stores/following.store';
 import { storeToRefs } from 'pinia';
-import { useDocumentVisibility, useWindowSize } from '@vueuse/core';
+import { useDocumentVisibility, useWindowFocus, useWindowSize } from '@vueuse/core';
 
 TitleService.setTitle('Live');
 const favourtieStore = useFavouriteStore();
 const followingStore = useFollowingStore();
+const focused = useWindowFocus();
 const twitchApiService = new TwitchApiService();
 
 const { filter } = storeToRefs(followingStore);
@@ -22,38 +23,45 @@ const favouriteIds = ref<number[]>(favourtieStore.getFavouriteStreamers());
 const allStreams = ref<TwitchFollowedStreamWithUser[]>();
 const favouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
 const nonFavouriteStreams = ref<TwitchFollowedStreamWithUser[]>();
-const fetchHistory = ref<string[]>([]);
+const lastFetched = ref<number>(0);
 
 onMounted(async () => {
     fetchStreams();
-    autoRefetchStreams();
+    autoRefetchStreamsInterval();
 });
 
-const isMobile = computed(() => {
-    const { width } = useWindowSize();
-    return width.value < 1000;
-});
-
-const autoRefetchStreams = () => {
+const autoRefetchStreamsInterval = () => {
     const fourSec = 4 * 1000;
     const fourMins = 4 * 60 * 1000;
     const twoMin = 2 * 60 * 1000;
 
     setInterval(() => {
-        if (isMobile.value) return;
+        if (!focused.value) return;
         fetchStreams();
     }, twoMin);
 };
 
 const fetchStreams = async () => {
-    const str = `Fetcing streams..: ${new Date()}`;
-    console.log(str);
-    fetchHistory.value.push(str);
+    console.log(`Fetcing streams..: ${new Date()}`);
+    lastFetched.value = Date.now();
 
     allStreams.value = await twitchApiService.getFollowedStreamsWithUsers();
     favouriteStreams.value = LiveService.getFavourites(favouriteIds.value, allStreams.value);
     nonFavouriteStreams.value = LiveService.getNonFavourites(favouriteIds.value, allStreams.value);
 };
+
+const refetchStreamsOnFocus = (isFocued: boolean) => {
+    if (!isFocued) return;
+
+    const isLongerThan10SecAgo = Date.now() - lastFetched.value > 10 * 1000;
+    if (!isLongerThan10SecAgo) return;
+
+    fetchStreams();
+};
+
+watch(focused, (focused) => {
+    refetchStreamsOnFocus(focused);
+});
 </script>
 
 <template>
