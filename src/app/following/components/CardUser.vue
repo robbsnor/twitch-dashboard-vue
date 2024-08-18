ec2f8fae423741ae49b312166a3632ee78cb3fd0
Robbin Schmidt <robbinschmidt@gmail.com>
2024-08-18 20:54:46 +0200

    -

--- src/app/following/components/CardUser.vue
+++ src/app/following/components/CardUser.vue
@@ -14,9 +14,9 @@ const link = computed(() => `/user/${props.name}`);
 
 <template>
     <RouterLink :to="link" class="card-user">
-        <v-img :src="props.avatar" :alt="`${props.name}'s avatar`" class="card-user__avatar" />
+        <img :src="props.avatar" :alt="`${props.name}'s avatar`" class="card-user__avatar" />
         <h3 class="card-user__name">{{ name }}</h3>
-        <v-img :src="props.backgroundImage" :alt="`${props.name}'s' offline image`" class="card-user__background-image"/>
+        <img :src="props.backgroundImage" :alt="`${props.name}'s' offline image`" class="card-user__background-image"/>
     </RouterLink>
 </template>
 
