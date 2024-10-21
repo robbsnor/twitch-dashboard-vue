19da0fdf296d73cc29ab491e2ef0c4941da4713e
Robbin Schmidt <robbinschmidt@gmail.com>
2024-10-21 07:52:19 +0200

    -

--- src/app/following/containers/LivePage.vue
+++ src/app/following/containers/LivePage.vue
@@ -30,10 +30,10 @@ watch(focused, async (isFocused) => {
     if (!isFocused) return;
     if (!lastFetchedOn.value) return;
 
-    const isLongerThan30SecAgo = new Date().getTime() - lastFetchedOn.value > 1000 * 30; // 30 og
+    const isLongerThan30SecAgo = new Date().getTime() - lastFetchedOn.value > 1000 * 30;
     if (!isLongerThan30SecAgo) return;
 
-    toast.success(`Refreshing...`, { duration: 1500 });
+    toast.success(`Refreshing...`, { duration: 2000 });
     streams.value = await FollowingFacade.getStreams();
 });
 
