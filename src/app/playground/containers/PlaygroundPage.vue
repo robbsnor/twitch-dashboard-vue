a00ddeec94bd58bb9d64270903f15af505319a65
Robbin Schmidt <robbinschmidt@gmail.com>
2024-10-23 01:41:09 +0200

    -

--- src/app/playground/containers/PlaygroundPage.vue
+++ src/app/playground/containers/PlaygroundPage.vue
@@ -1,5 +1,57 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { onMounted, ref } from "vue";
+import gsap from "gsap";
+
+onMounted(() => {
+    gsapGo();
+});
+
+const gsapGo = () => {
+    const centerX = window.innerWidth / 2;
+    const tl = gsap.timeline({ repeat: -1, repeatDelay: 1 });
+    const initialWidth = Math.ceil(document.querySelector('.box')!.getBoundingClientRect().width);
+    const initialHeight = Math.ceil(document.querySelector('.box')!.getBoundingClientRect().height);
+
+    // set initial position
+    tl.set(".box", {
+        y: window.innerHeight,
+        x: centerX,
+        width: 0,
+        height: 0,
+        padding: 0,
+    });
+
+    tl.set('.box__inner', {
+        opacity: 0,
+        translateY: -10,
+    });
+
+    // bounce up
+    tl.to(".box", {
+        y: '-=50px',
+        duration: .5,
+        width: 20,
+        opacity: 1,
+        height: 20,
+        left: -10,
+        top: -10,
+        ease: 'back.out',
+    });
+
+    // end
+    tl.to(".box", {
+        width: initialWidth,
+        height: initialHeight,
+        duration: .5,
+        ease: 'expo',
+    }, '-=.1');
+
+    tl.to(".box__inner", {
+        opacity: 1,
+        translateY: 0,
+        duration: .2,
+    }, '-=.4');
+};
 </script>
 
 <template>
@@ -9,8 +61,28 @@ import { ref } from "vue";
 
         </Section>
     </div>
+
+    <div class="box">
+        <div class="box__inner">Big booty bitches in the house!</div>
+    </div>
 </template>
 
 <style scoped lang="scss">
 .playground {}
+
+.box {
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: $c-primary;
+    color: black;
+    padding: 10px 30px;
+    border-radius: 999px;
+    transform: translate(-50%, -50%);
+
+    &__inner {
+        text-align: center;
+    }
+}
 </style>
