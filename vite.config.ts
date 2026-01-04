73fb7b85426524599994ea86b72af49b283ba38c
robbsnor <robbinschmidt@gmail.com>
2026-01-04 02:44:49 +0100

    -

--- vite.config.ts
+++ vite.config.ts
@@ -21,17 +21,12 @@ export default defineConfig({
             includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
             manifest: {
                 name: 'Twitch Dashboard',
-                short_name: 'Dashboard',
+                short_name: 'Twitch Dashboard',
                 display: 'standalone',
                 theme_color: '#000000',
-                background_color: '#000000',
+                background_color: 'transparent',
                 start_url: '/following/live',
                 icons: [
-                    {
-                        src: 'pwa-64x64.png',
-                        sizes: '64x64',
-                        type: 'image/png',
-                    },
                     {
                         src: 'pwa-192x192.png',
                         sizes: '192x192',
@@ -41,13 +36,6 @@ export default defineConfig({
                         src: 'pwa-512x512.png',
                         sizes: '512x512',
                         type: 'image/png',
-                        purpose: 'any',
-                    },
-                    {
-                        src: 'maskable-icon-512x512.png',
-                        sizes: '512x512',
-                        type: 'image/png',
-                        purpose: 'maskable',
                     },
                 ],
             },
