609b230fdfaf33360f2af7eaf8618c5f607c9bc8
robbsnor <robbinschmidt@gmail.com>
2026-01-05 04:56:15 +0100

    -

--- src/app/following/components/CardLiveFancy.vue
+++ src/app/following/components/CardLiveFancy.vue
@@ -30,7 +30,7 @@ const uptime = computed(() => CardLiveService.getUptime(props.card.startedAt));
         <div v-if="card.game" class="line-clamp-1 w-full shrink-0 text-[38px] font-bold">
             {{ card.game }}
         </div>
-        <div class="text-black-1900 mb-3 line-clamp-1 w-full pt-1.5 text-[18px] font-bold">
+        <div class="text-black-1900 mb-3 line-clamp-1 w-full text-[18px] font-bold">
             {{ card.title }}
         </div>
 
