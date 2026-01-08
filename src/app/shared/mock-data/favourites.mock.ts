e752f6b8582ee665b1ceb0342aa167ad25d3affa
robbsnor <robbinschmidt@gmail.com>
2026-01-08 21:42:26 +0100

    -

--- src/app/shared/mock-data/favourites.mock.ts
+++ src/app/shared/mock-data/favourites.mock.ts
@@ -1,22 +1,22 @@
 export const MOCK_FAVOURITES_HOPP = [
     52385053, // lekkerspelen
     603776329, // ariathome
-    128856353, // mande
-    504661144, // DJ_Dave
     149141153, // slewya
+    504661144, // DJ_Dave
+    128856353, // mande
+    41157071, // tangotek
     629320451, // infs___
     57357891, // furretcanwalk
-    239356777, // tailscanfly
     26991127, // hiko
-    41157071, // tangotek
+    51929371, // ml7support
+    218059024, // spawnsgg
     43683025, // ohnepixel
     70225218, // tenz
+    239356777, // tailscanfly
     912918849, // 223lab, ruski
-    218059024, // spawnsgg
     183796200, // kraan
     102220212, // xxLUSHx
     101936909, // stormen
-    51929371, // ml7support
     31239503, // ESLCS
     19840041, // sheepolution
     23220337, // emongg
