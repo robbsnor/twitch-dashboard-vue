0c9fc0664ce835abb63fef2039923516c086665a
robbsnor <robbinschmidt@gmail.com>
2026-01-15 04:59:13 +0100

    -

--- src/app/auth/containers/SignInPage.vue
+++ src/app/auth/containers/SignInPage.vue
@@ -4,18 +4,20 @@ import { useAuthStore } from '../stores/auth.store';
 import { useRouter } from 'vue-router';
 import { supabase } from '@/app/supabase';
 import { PromiseService } from '@/app/shared/services/promise.service';
+import { useFavouriteStore } from '@/app/shared/stores/favourites.store';
 
 const router = useRouter();
 const authStore = useAuthStore();
+const favouriteStore = useFavouriteStore();
 
 onMounted(async () => {
     const {
         data: { session },
     } = await supabase.auth.getSession();
-
     if (!session?.user) return;
 
     authStore.setSession(session);
+    await favouriteStore.init();
 
     router.push({ name: 'live' });
 });
