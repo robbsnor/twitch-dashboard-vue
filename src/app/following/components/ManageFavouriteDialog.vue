7efbe091acfcbd1ca3673a1c8e26538f5e6eade2
robbsnor <robbinschmidt@gmail.com>
2026-01-16 13:09:21 +0100

    -

--- src/app/following/components/ManageFavouriteDialog.vue
+++ src/app/following/components/ManageFavouriteDialog.vue
@@ -19,18 +19,13 @@ const twitchApi = useTwitchApi();
 const dialog = defineModel<boolean>();
 const loading = ref(false);
 const saving = ref(false);
-const props = withDefaults(
-    defineProps<{
-        stream: TwitchFollowedStreamWithUser;
-        editmode?: boolean;
-    }>(),
-    {
-        editmode: false,
-    }
-);
-const form = ref<AddFavourtieUserProps[]>([]);
+const props = defineProps<{
+    stream: TwitchFollowedStreamWithUser;
+    editmode?: boolean;
+}>();
+const users = ref<TwitchUser[]>([]);
 // const description = `Add "${props.stream.user_name}" to your favourites`;
-const description = `ffffs`;
+const description = `Description coming soon...`;
 
 async function onOpen() {
     loading.value = true;
@@ -38,35 +33,21 @@ async function onOpen() {
     await PromiseService.sleep(200);
     const res = await twitchApi.getUsers({ ids: favouriteStore.favouriteUserIds });
 
-    form.value = res.data.map((user) => ({
-        name: user.display_name,
-        avatar: user.profile_image_url,
-        id: Number(user.id),
-    }));
+    users.value = res.data;
 
     loading.value = false;
 }
 
 async function insertBelow(index: number) {
     // remove if exists
-    const existingIndex = form.value.findIndex((u) => u.id === Number(props.stream.user_id));
-    if (existingIndex !== -1) {
-        form.value.splice(existingIndex, 1);
-        if (existingIndex < index) {
-            index--;
-        }
-    }
+    users.value = users.value.filter((u) => u.id !== props.stream.user_id);
 
     // add
-    form.value.splice(index + 1, 0, {
-        name: props.stream.user_name,
-        avatar: props.stream.profile_image_url,
-        id: Number(props.stream.user_id),
-    });
+    users.value.splice(index + 1, 0, props.stream);
 }
 
 function removeFavourite(index: number) {
-    form.value.splice(index, 1);
+    users.value.splice(index, 1);
 }
 
 async function save() {
@@ -74,7 +55,7 @@ async function save() {
         saving.value = true;
 
         await PromiseService.sleep(1000);
-        await favouriteStore.setFavouriteUsers(form.value.map((u) => u.id));
+        await favouriteStore.setFavouriteUsers(users.value.map((u) => Number(u.id)));
     } finally {
         saving.value = false;
         dialog.value = false;
@@ -86,23 +67,20 @@ async function save() {
     <Dialog v-model="dialog" title="Favourites" :description="description" icon="mdi-heart" @open="onOpen" width="500">
         <template v-if="!loading">
             <div class="flex flex-col max-h-150">
-                <VueDraggable handle="._handle" :animation="100" v-model="form">
+                <VueDraggable handle="._handle" :animation="100" v-model="users">
                     <div
-                        v-for="(user, index) in form"
+                        v-for="(user, index) in users"
                         :key="user.id"
-                        :class="{ 'bg-black-400 rounded': user.id === Number(props.stream.user_id) }"
+                        :class="{ 'bg-black-400 rounded': user.id === props.stream.user_id }"
                         class="flex items-center gap-4 py-2 px-4 bg-black-200 not-last:border-b border-black-500"
                     >
                         <v-icon icon="mdi-drag" class="_handle cursor-move" color="var(--color-black-800)" />
                         <!-- <div class="text-muted font-bold text-right">
                             {{ index + 1 }}
                         </div> -->
-                        <img :src="user.avatar" alt="avatar" class="size-10 rounded-full" />
-                        <div
-                            class="font-bold truncate"
-                            :class="{ 'text-primary': user.id === Number(props.stream.user_id) }"
-                        >
-                            {{ user.name }}
+                        <img :src="user.profile_image_url" alt="avatar" class="size-10 rounded-full" />
+                        <div class="font-bold truncate" :class="{ 'text-primary': user.id === props.stream.user_id }">
+                            {{ user.display_name }}
                         </div>
                         <div class="ml-auto flex gap-4 items-center">
                             <v-btn
@@ -116,7 +94,7 @@ async function save() {
                             ></v-btn>
 
                             <v-btn
-                                v-if="user.id !== Number(props.stream.user_id) && !props.editmode"
+                                v-if="user.id !== props.stream.user_id && !props.editmode"
                                 icon="mdi-arrow-left-bottom"
                                 size="small"
                                 variant="tonal"
